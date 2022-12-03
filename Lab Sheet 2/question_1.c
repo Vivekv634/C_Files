91c3cab69c99ffbcf55eab2d84399c78b631d5d5
Vivek Vaish <vaishvivek634@gmail.com>
2022-12-03 23:14:01 +0530

    modifying files

--- Lab Sheet 2/question_1.c
+++ Lab Sheet 2/question_1.c
@@ -4,12 +4,7 @@ int main()
 {
     int a, b;
     float c;
-    printf("Enter first integer value : ");
-    scanf("%d", &a);
-    printf("Enter second integer value : ");
-    scanf("%d", &b);
-    printf("Enter float value : ");
-    scanf("%f", &c);
+    a = 10, b = 15, c = 12.6;
     printf("First Integer value : %d\nSecond Integer value : %d\nFloat value : %.02f", a, b, c);
     return 0;
 }
--- Lab Sheet 2/question_3.c
+++ Lab Sheet 2/question_3.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    float principal, rate, time, simple_interest;
-    printf("Enter the value of principal : ");
-    scanf("%f", principal);
-    printf("Enter the value of time : ");
-    scanf("%f", time);
-    printf("Enter the value of rate : ");
-    scanf("%f", rate);
-    simple_interest = principal*(1+ rate*time);
-    printf("Simple Interest : %f", simple_interest);
+    float principal, rate, time, CI, SI;
+    printf("Enter principal value : ");
+    scanf("%f", &principal);
+    printf("Enter rate value : ");
+    scanf("%f", &rate);
+    printf("Enter time value : ");
+    scanf("%f", &time);
+    SI = (principal * rate * time) / 100;
+    CI = principal * pow((1 + rate / 100), time);
+    printf("Simple Interest : %.3f\n", SI);
+    printf("Compound Interest : %.3f", CI);
     return 0;
 }
+
